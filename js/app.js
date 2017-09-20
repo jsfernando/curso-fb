62070a6990fbccea13456c0d4e5ac49b31ebd748
jsfernando <jsfernando@gmail.com>
2017-09-20 14:56:34 -0300

    meu primeiro commit

---
+++ js/app.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyAMJtG9PrOZqaIDmrPcrGPWZf-mCnesIZE",
+    authDomain: "curso-fb-d48d8.firebaseapp.com",
+    databaseURL: "https://curso-fb-d48d8.firebaseio.com",
+    projectId: "curso-fb-d48d8",
+    storageBucket: "curso-fb-d48d8.appspot.com",
+    messagingSenderId: "670483545505"
+};
+firebase.initializeApp(config);
