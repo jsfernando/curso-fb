cc1b2edecd7c20d8655af629bdea24d76f8de9dd
jsfernando <jsfernando@gmail.com>
2018-02-21 12:00:40 -0300

    storage ok!

--- js/app.js
+++ js/app.js
@@ -4,7 +4,7 @@ var config = {
     authDomain: "curso-fb-d48d8.firebaseapp.com",
     databaseURL: "https://curso-fb-d48d8.firebaseio.com",
     projectId: "curso-fb-d48d8",
-    storageBucket: "curso-fb-d48d8.appspot.com",
+    storageBucket: "gs://curso-fb-d48d8.appspot.com/",
     messagingSenderId: "670483545505"
 };
 firebase.initializeApp(config);
